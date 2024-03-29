96d5b94dee5efb082f57a434412f73d1ef1ac3c4
HA <hoanganhkemosabe@gmail.com>
2024-03-29 18:08:42 +0700

    done

--- base/views.py
+++ base/views.py
@@ -1,7 +1,24 @@
+import csv
+from io import StringIO
 from django.shortcuts import render
-
-# Create your views here.
+from .models import WeatherData
 
 def home(request):
     context = {}
     return render(request, 'base/home.html', context)
+
+
+def test(request):
+    weather_data = WeatherData.objects.all()
+    csv_data = []
+    for data in weather_data:
+
+        csv_file = data.csv_file.read().decode('utf-8')
+
+        csv_reader = csv.reader(StringIO(csv_file))
+
+        next(csv_reader)
+        for row in csv_reader:
+            csv_data.append(row)
+    context = {'csv_data': csv_data}
+    return render(request, 'base/test.html', context)
